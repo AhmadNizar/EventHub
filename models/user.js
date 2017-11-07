82421c708bd7138e316577fa534a84ac9ee9ec5c
AhmadNizar <ahmadnizar.owl@gmail.com>
2017-11-07 16:19:34 +0700

    add constraint unique to username

--- models/user.js
+++ models/user.js
@@ -3,7 +3,13 @@ module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
-    username: DataTypes.STRING,
+    username: {
+      type : DataTypes.STRING,
+      unique : {
+        args : true,
+        msg : 'Username sudah terdaftar'
+      }
+    },
     password: DataTypes.STRING,
     email: {
       type : DataTypes.STRING,
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router  = express.Router()
+const Models  = require('../models')
 
 router.get('/', (req, res) => {
 	res.render('index')
@@ -7,6 +8,16 @@ router.get('/', (req, res) => {
 
 router.get('/signup', (req, res)=> {
 	res.render('signup')
+})
+
+router.post('/signup', (req, res)=> {
+	Models.User.create({
+		first_name : req.body.first_name, 
+		last_name : req.body.last_name, 
+		username : req.body.username, 
+		password : req.body.password,
+		email : req.body.email
+	})
 })
 
 module.exports = router
